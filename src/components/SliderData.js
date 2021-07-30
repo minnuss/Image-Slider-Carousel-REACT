75caf42c6801342cc57e6616aff1bd65edde7696
minnuss <ponjevic.goran@hotmail.com>
2021-07-30 21:00:52 +0200

    initial commit

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,20 +1,20 @@
 export const SliderData = [
     {
-        image: '../images/1.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/1.jpeg'
     },
     {
-        image: '../images/2.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/2.jpeg'
     },
     {
-        image: '../images/3.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/3.jpeg'
     },
     {
-        image: '../images/4.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/4.jpeg'
     },
     {
-        image: '../images/5.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/5.jpeg'
     },
     {
-        image: '../images/6.jpeg'
+        image: 'Image-Slider-Carousel-REACT/images/6.jpeg'
     }
 ]
